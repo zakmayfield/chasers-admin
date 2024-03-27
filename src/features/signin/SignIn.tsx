6c13b8046bcfa64c58dce7a469ff409a7c0fde53
Zak Mayfield <zakmayfield@gmail.com>
2024-03-27 13:38:17 -0400

    config: boilerplate components

---
+++ src/features/signin/SignIn.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { FC } from 'react';
+
+interface SignInProps {}
+
+export const SignIn: FC<SignInProps> = ({}) => {
+  return <div>SignIn</div>;
+};
---
+++ src/features/signup/SignUp.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { FC } from 'react';
+
+interface SignUpProps {}
+
+export const SignUp: FC<SignUpProps> = ({}) => {
+  return <div>SignUp</div>;
+};
