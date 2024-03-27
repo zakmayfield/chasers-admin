cc7ea96f52dfb4d7bf47da7327ff5166f37591e8
Zak Mayfield <zakmayfield@gmail.com>
2024-03-27 13:43:52 -0400

    config: body height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={quicksand.className}>
-        <main>
+        <main className='min-h-screen'>
           <Providers>
             {children}
             <ReactQueryDevtools initialIsOpen={false} />
