736c9b4ade25c21df9a79e7cc4f675c475207c9a
Zak Mayfield <zakmayfield@gmail.com>
2024-03-27 13:41:00 -0400

    config: root page to `SignIn`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Button } from '../shared/components';
+import { SignIn } from '@/features';
 
 export default function Home() {
-  return (
-    <div>
-      <h1>root</h1>
-
-      <Button content='Try Me' />
-    </div>
-  );
+  return <SignIn />;
 }
