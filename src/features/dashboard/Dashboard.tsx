1cd930d68b9d4eab7ad9fd04c06a2ea5e6ece764
Zak Mayfield <zakmayfield@gmail.com>
2024-04-02 17:56:46 -0400

    chore: general cleanup

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -15,10 +15,8 @@ export const Dashboard: FC<DashboardProps> = ({}) => {
       <Overview className='border col-span-4' />
       <NewOrders className='border col-span-2' />
       <NewUsers className='border col-span-2' />
+
       <ContainerFull className='border col-span-4'>
-        <h2>placeholder</h2>
-      </ContainerFull>
-      <ContainerFull className='border col-start-3 col-span-4 h-72'>
         <h2>placeholder</h2>
       </ContainerFull>
     </DashboardLayout>
