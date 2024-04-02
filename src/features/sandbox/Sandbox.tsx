e714b81e5574899544023fe0610fe5397feac215
Zak Mayfield <zakmayfield@gmail.com>
2024-04-02 11:39:18 -0400

    chore: use styled links

--- src/features/sandbox/Sandbox.tsx
+++ src/features/sandbox/Sandbox.tsx
@@ -1,3 +1,4 @@
+import { StyledLink } from '@/shared/components';
 import Link from 'next/link';
 import { FC } from 'react';
 
@@ -6,8 +7,11 @@ interface SandboxProps {}
 export const Sandbox: FC<SandboxProps> = ({}) => {
   return (
     <div>
-      <nav>
-        <Link href='/sandbox/container'>container</Link>
+      <h1 className='mb-6'>Components</h1>
+
+      <nav className='flex gap-3'>
+        <StyledLink path='/sandbox/container' content='container' />
+        <StyledLink path='/sandbox/grid' content='grid' />
       </nav>
     </div>
   );
