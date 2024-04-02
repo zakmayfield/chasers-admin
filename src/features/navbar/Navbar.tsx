e03384335e1612207cc7c7b2911867f68ff21795
Zak Mayfield <zakmayfield@gmail.com>
2024-04-02 16:22:06 -0400

    feat: navbar structure

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,9 +1,30 @@
 'use client';
 
+import {
+  Container,
+  ContainerFull,
+  FlexCol,
+  FlexRow,
+  StyledLink,
+} from '@/shared/components';
+import { merge } from '@/utils';
 import { FC } from 'react';
 
-interface NavbarProps {}
+interface NavbarProps {
+  className?: string;
+  underline?: boolean;
+}
 
-export const Navbar: FC<NavbarProps> = ({}) => {
-  return <div>Navbar</div>;
+export const Navbar: FC<NavbarProps> = ({ className, underline }) => {
+  return (
+    <Container className={merge(`flex items-center ${className ?? ''}`)}>
+      <nav className={`w-full ${underline && 'underline'}`}>
+        <FlexRow className='justify-end'>
+          <StyledLink path='#' content='users' />
+          <StyledLink path='#' content='orders' />
+          <StyledLink path='#' content='logout' />
+        </FlexRow>
+      </nav>
+    </Container>
+  );
 };
