165cd7ce6d31d70b639511ca5d058b0765bd9a11
Zak Mayfield <zakmayfield@gmail.com>
2024-04-06 10:18:16 -0400

    config: URL constants and utils

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,10 @@
+//^ URLS
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+//^ PATHS
+export const paths = {
+  dashbard: '/dashboard',
+  users: '/users',
+  orders: '/orders',
+  admins: '/admins',
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,11 @@
+import { BASE_URL } from '@/shared/constants';
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function merge(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function getRoute(path: string) {
+  return BASE_URL + path;
 }
