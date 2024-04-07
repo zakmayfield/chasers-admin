a74f6a2ce5cd7be03b5781f3fad7b554941dd926
Zak Mayfield <zakmayfield@gmail.com>
2024-04-07 10:14:50 -0400

    feat: component setup

--- src/features/admins/components/authorized-admins/AuthorizedAdmins.tsx
+++ src/features/admins/components/authorized-admins/AuthorizedAdmins.tsx
@@ -1,7 +1,15 @@
+import { ContainerFull } from '@/shared/components';
+import { merge } from '@/utils';
 import { FC } from 'react';
 
-interface AuthorizedAdminsProps {}
+interface AuthorizedAdminsProps {
+  className?: string;
+}
 
-export const AuthorizedAdmins: FC<AuthorizedAdminsProps> = ({}) => {
-  return <div>AuthorizedAdmins</div>;
+export const AuthorizedAdmins: FC<AuthorizedAdminsProps> = ({ className }) => {
+  return (
+    <ContainerFull className={merge(`${className}`)}>
+      <h2>Authorized Admins</h2>
+    </ContainerFull>
+  );
 };
