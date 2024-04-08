6866b5d32dc36726606c3091f97a4a1ba8fc8683
Zak Mayfield <zakmayfield@gmail.com>
2024-04-08 12:40:27 -0400

    chore: general

--- src/features/admins/components/authorized-admins/AuthorizedAdmins.tsx
+++ src/features/admins/components/authorized-admins/AuthorizedAdmins.tsx
@@ -1,5 +1,8 @@
 import { ContainerFull } from '@/shared/components';
+import { getAuthorizedAdmins } from '@/shared/services/queries/admins/getAuthorizedAdmins';
+import { QueryKeys } from '@/shared/types';
 import { merge } from '@/utils';
+import { useQuery } from '@tanstack/react-query';
 import { FC } from 'react';
 
 interface AuthorizedAdminsProps {
@@ -7,6 +10,11 @@ interface AuthorizedAdminsProps {
 }
 
 export const AuthorizedAdmins: FC<AuthorizedAdminsProps> = ({ className }) => {
+  const {} = useQuery<any, Error>({
+    queryKey: [QueryKeys.AUTHORIZED_ADMINS],
+    queryFn: getAuthorizedAdmins,
+  });
+
   return (
     <ContainerFull className={merge(`${className}`)}>
       <h2>Authorized Admins</h2>
