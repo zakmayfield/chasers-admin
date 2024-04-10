bb396bfd3f039b7b333c33ec61ea5b776f109a0c
Zak Mayfield <zakmayfield@gmail.com>
2024-04-10 10:20:39 -0400

    chore: cleanup

--- src/shared/constants/constants.ts
+++ src/shared/constants/constants.ts
@@ -1,10 +1,6 @@
-import { QueryKeys } from '@/shared/types';
-import { Paths } from '@/shared/types';
+import { Paths, QueryKeys } from '@/shared/types';
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-// TODO: implement on middleware matcher
-const matchingPaths = ['/dashboard', '/orders', '/users', '/admins'];
 
 export const paths: Paths = {
   dashboard: '/dashboard',
